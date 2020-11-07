affff17a5dffb724697824f18a6df23fcf368983
jonas <jonas@ebg.pw>
2020-11-07 18:03:53 +0100

    Initial commit

---
+++ example.secret.py
@@ -0,0 +1,6 @@
+API_ID = 33
+API_HASH = '33'
+TOKEN = '1411733BjrHpB8mcmqDg'
+NAME = TOKEN.split(':')[0]
+ebg_token = '2MmsaOKA33rv7kcmAYkiYptbsQ'
+sw_token = 'XVe33~MxC1RZTabLEkF'
---
+++ main.py
@@ -0,0 +1,70 @@
+# This is a sample Python script.
+import asyncio
+from kantex.md import *
+from telethon import TelegramClient, events
+from telethon.events import NewMessage
+from secret import *
+
+import spamwatch
+from telethon.tl import functions, types
+from telethon.tl.custom import Message
+
+client = spamwatch.Client(ebg_token,
+                          host='https://kantek.eule.computer')
+swclient = spamwatch.Client(sw_token)
+
+
+
+
+async def main():
+    bot = TelegramClient(NAME, API_ID, API_HASH)
+
+    await bot.start(bot_token=TOKEN)
+
+    @bot.on(events.NewMessage(pattern='/start', forwards=False))
+    async def start(event):
+        await event.respond(f'Helluwu, \n'
+                            f'you can check if an id is banned by using /check [userid]')
+
+    @bot.on(events.NewMessage(pattern='/ebg', forwards=False))
+    async def check(event):
+        msg: Message = event.message
+        text: str = msg.raw_text
+        id = text.split(' ')[1]
+        ban = client.get_ban(id)
+        if not ban:
+            await event.respond(f' The ID {id} is not banned in the EBG Network')
+            return
+
+        message = f'The User with the ID {Code(id)} is banned!\n' \
+                  f'' \
+                  f'Time of the Ban: {Code(ban.date)}\n' \
+                  f'Reason for the Ban: {Code(ban.reason)}\n' \
+                  f'Offending Message (if on record):\n {Code(ban.message) if ban.message else Code("Fehlt")}\n' \
+                  f'\n\n\nPowered by @GodOfOwls'
+
+        await event.respond(message)
+
+    @bot.on(events.NewMessage(pattern='/spamwatch', forwards=False))
+    async def check(event):
+        msg: Message = event.message
+        text: str = msg.raw_text
+        id = text.split(' ')[1]
+        ban = swclient.get_ban(id)
+        if not ban:
+            await event.respond(f' The ID {id} is not banned in SpamWatch')
+            return
+
+        message = f'The User with the ID {Code(id)} is banned!\n' \
+                  f'' \
+                  f'Time of the Ban: {Code(ban.date)}\n' \
+                  f'Reason for the Ban: {Code(ban.reason)}\n' \
+                  f'Offending Message (if on record):\n {Code(ban.message) if ban.message else Code("Fehlt")}\n' \
+                  f'\n\n\nPowered by @GodOfOwls\n using the official @Spamwatch API'
+        await event.respond(message)
+
+    await bot.run_until_disconnected()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
